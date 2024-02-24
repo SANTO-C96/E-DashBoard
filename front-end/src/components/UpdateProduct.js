8dbf95bd4f56d92c9203bf5fbd0c77e26ed72fc5
SANTOSH KUMAR SINGH <89244724+SANTO-C96@users.noreply.github.com>
2024-02-24 14:29:43 +0530

    Update UpdateProduct.js
    changed in the api url of updateProduct.js

--- front-end/src/components/UpdateProduct.js
+++ front-end/src/components/UpdateProduct.js
@@ -16,7 +16,7 @@ const UpdateProduct = () => {
 
     const getProductDetails = async () =>{
         console.warn(params);
-        let result = await fetch(`http://localhost:5000/product/${params.id}`);
+        let result = await fetch(`https://e-dash-board-api.vercel.app/product/${params.id}`);
         result = await result.json();
         setName(result.name);
         setPrice(result.price);
@@ -25,7 +25,7 @@ const UpdateProduct = () => {
     }
     const updateProduct=async()=>{
         console.warn(name, price, category, company);
-        let result = await fetch(`http://localhost:5000/product/${params.id}`,{
+        let result = await fetch(`https://e-dash-board-api.vercel.app/product/${params.id}`,{
             method:'put',
             body:JSON.stringify({name, price, category, company}),
             headers:{
